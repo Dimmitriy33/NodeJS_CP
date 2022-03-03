3605bd441d06bdf5c607c135bb54c1d0bd585050
Dmitry <61908324+Dimmitriy33@users.noreply.github.com>
2022-03-03 18:15:22 +0300

    Backend. Add salt for user password

--- config/default.ts
+++ config/default.ts
@@ -3,5 +3,6 @@ import 'dotenv/config';
 export default {
     port: process.env.PORT,
     dbUri: process.env.MONGO_URL,
-    env: process.env.NODE_ENV // prod or dev
+    env: process.env.NODE_ENV, // prod or dev
+    saltWorkFactor: 10, // how many rounds should you salt the password
 };
--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -1,40 +1,56 @@
 import mongoose from 'mongoose';
+import bcrypt from 'bcrypt';
+import config from 'config';
 
 export interface UserDocument extends mongoose.Document {
-    name: string;
-    email: string;
-    password: string;
-    createdAt: Date; // related to timestamps
-    updatedAt: Date; // related to timestamps
+  name: string;
+  email: string;
+  password: string;
+  createdAt: Date; // related to timestamps
+  updatedAt: Date; // related to timestamps
+  comparePassword(inPass: string): Promise<boolean>;
 }
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    password: {
-        type: String,
-        required: true
-    }
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  }
 }, {
-    timestamps: true
+  timestamps: true
 });
 
-userSchema.pre("save", async function(next: mongoose.HookNextFunction) {
-    let user = this as UserDocument;
+userSchema.pre("save", async function(next) {
+  let user = this as UserDocument;
 
-    if(!user.isModified("password")) {
-        return next();
-    }
+  if(!user.isModified("password")) {
+    return next();
+  }
 
-    const salt = await bcrypt.genSalt(10);
+  const saltWorkFactor = config.get<number>("saltWorkFactor");
+  const salt = await bcrypt.genSalt(saltWorkFactor);
+  const hash = bcrypt.hashSync(user.password, salt);
+
+  user.password = hash;
+  
+  return next();
 });
+
+userSchema.methods.comparePassword = async function(inPass: string): Promise<boolean> {
+  const user = this as UserDocument;
+  return bcrypt
+        .compare(inPass, user.password)
+        .catch((e) => false);
+}
 
 // tslint:disable-next-line: variable-name
 const UserModel = mongoose.model('User', userSchema);
