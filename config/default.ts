a9accd3fb34af435a6817b2ed846f79497ed78f5
Dmitry <61908324+Dimmitriy33@users.noreply.github.com>
2022-03-04 19:01:47 +0300

    Backend. Config. Add default.example.ts and rm default.ts

--- config/default.ts
+++
@@ -1,8 +0,0 @@
-import 'dotenv/config';
-
-export default {
-    port: process.env.PORT,
-    dbUri: process.env.MONGO_URL,
-    env: process.env.NODE_ENV, // prod or dev
-    saltWorkFactor: 10, // how many rounds should you salt the password
-};
