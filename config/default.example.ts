eeed39543294731db7ecc4315a82eaa80c6fa6ec
Dmitry <61908324+Dimmitriy33@users.noreply.github.com>
2022-05-06 21:42:18 +0300

    Backend. Add desc for config

--- config/default.example.ts
+++ config/default.example.ts
@@ -1,18 +1,22 @@
 import 'dotenv/config';
 
 export default {
-    port: process.env.PORT, // if use .env
-    dbUri: '', // mongo Uri
-    env: 'development', // production or development
-    saltWorkFactor: 10, // how many rounds should you salt the password
-    accessTokenTtl: '1m',
-    refreshTokenTtl: '1y',
-    publicJWTKey: `-----BEGIN RSA PUBLIC KEY-----
+  port: process.env.PORT, // if use .env
+  dbUri: '', // mongo Uri
+  env: 'development', // production or development
+  saltWorkFactor: 10, // how many rounds should you salt the password
+  accessTokenTtl: '1m',
+  refreshTokenTtl: '1y',
+  publicJWTKey: `-----BEGIN RSA PUBLIC KEY-----
     Your public key
 -----END RSA PUBLIC KEY-----
 `,
-    privateJWTKey: `-----BEGIN RSA PRIVATE KEY-----
+  privateJWTKey: `-----BEGIN RSA PRIVATE KEY-----
     Your private key
 -----END RSA PRIVATE KEY-----
-`
+`,
+  cldName: 'yourCldName',
+  cldApiKey: 'apiKey',
+  cldApiSecret: 'apiSecret',
+  cldFolder: 'yourFolderName'
 };
