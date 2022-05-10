28af819f95f82ee1b32de2db643d82306e7746ba
Dmitry <61908324+Dimmitriy33@users.noreply.github.com>
2022-05-10 21:46:45 +0300

    Backend. Implement product service

---
+++ src/services/product.service.ts
@@ -0,0 +1,64 @@
+import ProductModel, { ProductDocument } from '../models/product.model';
+import { DocumentDefinition, FilterQuery } from 'mongoose';
+import { QueryOptions } from 'winston';
+
+export async function creaeteProduct(product: DocumentDefinition<Omit<ProductDocument, 'createdAt' | 'updatedAt'>>) {
+  return await ProductModel.create(product);
+}
+
+export async function findProduct(
+  query: FilterQuery<ProductDocument>, //
+  options?: QueryOptions
+) {
+  return await ProductModel.findOne(query, {}, options);
+}
+
+export async function findProductById(id: number) {
+  return await findProduct({
+    _id: id
+  });
+}
+
+export async function searchProductsByName(term: string, limit: number, offset: number) {
+  return await ProductModel.find(
+    {
+      name: { $regex: '^' + term }
+    },
+    {},
+    {
+      limit,
+      skip: offset
+    }
+  ).lean();
+}
+
+export async function getTopPopularPlatforms(count: number) {
+  return await ProductModel.aggregate([
+    {
+      $group: {
+        _id: '$platform',
+        count: { $sum: 1 }
+      },
+      $sort: {
+        count: -1
+      },
+      $limit: count
+    }
+  ]);
+}
+
+export async function updateProduct(
+  query: FilterQuery<ProductDocument>, //
+  update: DocumentDefinition<Omit<ProductDocument, 'createdAt' | 'updatedAt'>>, //
+  options?: QueryOptions
+) {
+  return await ProductModel.findOneAndUpdate(query, update, options);
+}
+
+export async function deleteProduct(id: number) {
+  return await ProductModel.findByIdAndDelete(id);
+}
+
+export async function softDeleteProduct(id: number) {
+  return await ProductModel.findByIdAndUpdate(id, { isDeleted: 1 });
+}
