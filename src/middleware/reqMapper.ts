a6c8cfc5de8c904c65da425011884eefd48df40a
Dmitry <61908324+Dimmitriy33@users.noreply.github.com>
2022-05-21 18:36:43 +0300

    Backend. Fix reqMapper for query

--- src/middleware/reqMapper.ts
+++ src/middleware/reqMapper.ts
@@ -1,11 +1,20 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { NextFunction, Request, Response } from 'express';
+import { isSymbol } from 'lodash';
 import { objKeysToFirstLetterLowerCase } from '../helpers/renameObjKeys';
 
 export default function reqMappper(req: Request, res: Response, next: NextFunction) {
   req.query = new Proxy(req.query, {
     //@ts-ignore
-    get: (target, name) => target[Object.keys(target).find((key) => key.toLowerCase() === name.toLowerCase())]
+    get: (target, name) =>
+      target[
+        //@ts-ignore
+        Object.keys(target).find((key) => {
+          const v1 = key.charAt(0).toLowerCase() + key.slice(1);
+          const v2 = isSymbol(name) ? name : name.charAt(0).toLowerCase() + name.slice(1);
+          return v1 === v2;
+        })
+      ]
   });
 
   // req.body = new Proxy(req.body, {
