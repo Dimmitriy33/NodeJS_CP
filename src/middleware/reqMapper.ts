afe5074a036e1a3b7f2f0075e53143cfdcf181c6
Dmitry <61908324+Dimmitriy33@users.noreply.github.com>
2022-05-21 18:07:08 +0300

    Backend. Update reqMapper middleware

--- src/middleware/reqMapper.ts
+++ src/middleware/reqMapper.ts
@@ -1,5 +1,6 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 import { NextFunction, Request, Response } from 'express';
+import { objKeysToFirstLetterLowerCase } from '../helpers/renameObjKeys';
 
 export default function reqMappper(req: Request, res: Response, next: NextFunction) {
   req.query = new Proxy(req.query, {
@@ -7,20 +8,20 @@ export default function reqMappper(req: Request, res: Response, next: NextFuncti
     get: (target, name) => target[Object.keys(target).find((key) => key.toLowerCase() === name.toLowerCase())]
   });
 
-  req.body = new Proxy(req.body, {
-    //@ts-ignore
-    get: (target, name) => target[Object.keys(target).find((key) => key.toLowerCase() === name.toLowerCase())]
-  });
+  // req.body = new Proxy(req.body, {
+  //   //@ts-ignore
+  //   get: (target, name) => target[Object.keys(target).find((key) => key.toLowerCase() === name.toLowerCase())]
+  // });
 
-  // if (Array.isArray(req.body)) {
-  //   req.body = req.body.map((item) => objKeysToLowerCase(item));
-  //   next();
-  //   return;
-  // }
+  if (Array.isArray(req.body)) {
+    req.body = req.body.map((item) => objKeysToFirstLetterLowerCase(item));
+    next();
+    return;
+  }
 
-  // if (typeof req.body === 'object') {
-  //   req.body = objKeysToLowerCase(req.body);
-  // }
+  if (typeof req.body === 'object') {
+    req.body = objKeysToFirstLetterLowerCase(req.body);
+  }
 
   next();
 }
